42221f07731874738ce74094c19543a481b2e33c
BalintCsala <balint.csala@ecosim.hu>
2021-03-18 14:48:08 +0100

    Added initial code

---
+++ src/projlab/skeleton/FunctionPrinter.java
@@ -0,0 +1,29 @@
+package projlab.skeleton;
+
+import java.util.ArrayList;
+
+public class FunctionPrinter {
+
+    private static int level = 0;
+
+    private static ArrayList<String> names = new ArrayList<>();
+
+    public static void enter(String className, String name) {
+        String n = className + "#" + name + "()";
+        System.out.println("    ".repeat(level) + n + " {");
+        names.add(n);
+        level++;
+    }
+
+    public static void msg(String msg) {
+        System.out.println("    ".repeat(level) + msg);
+    }
+
+    public static void exit() {
+        level--;
+        System.out.println("    ".repeat(level) + "} // " + names.remove(names.size() - 1));
+    }
+
+
+
+}
