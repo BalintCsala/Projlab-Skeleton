c3123dd17bf49d4683041b3073e0711a4070e9de
Ákos Regényi <regenyi.akos@edu.bme.hu>
2021-03-19 19:58:27 +0100

    watericejav

--- src/projlab/skeleton/WaterIce.java
+++ src/projlab/skeleton/WaterIce.java
@@ -7,5 +7,6 @@ public class WaterIce extends Resource {
 	public void reaction(Asteroid a) {
 		FunctionPrinter.enter("WaterIce", "reaction");
 		a.setResource(null);
+		FunctionPrinter.exit();
 	}
 }
